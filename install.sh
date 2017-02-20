c7c0d11e9a096c840653e553074684bf296e863e
Alex Becker <acbecker@uchicago.edu>
2017-02-19 19:40:51 -0800

    Make install.sh relative to framework directory.
    Also checks for existing install.

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-git clone "https://github.com/google/brotli"
-cd brotli
-mkdir out && cd out && ../configure-cmake && make
-make test
-make install
+cd local/plugins/brotli
+if [[ ! -e brotli ]]; then
+    git clone "https://github.com/google/brotli" || exit "Git clone failed. Check that git is installed and you are connected to the internet."
+    cd brotli
+    mkdir out && cd out && ../configure-cmake && make
+    make test
+    make install
+fi
